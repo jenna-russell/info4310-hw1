40db0993ca2fd6d84ef52f6a5bda584821292737
Jenna <jjr265@cornell.edu>
2020-09-22 20:25:44 -0400

    hi

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import *
 from whitenoise import WhiteNoise
-app = Flask(__name__)
+app = Flask(__name__, template_folder = static)
 app.wsi_app = WhiteNoise(app.wsgi_app, root='static/', prefix='static/', index_file="index.html", autorefresh=True)
 
 @app.route('/', methods=['GET'])
 def home():
-  return render_template("static/index.html")
+  return render_template("index.html")
 
 
 if __name__ == "__main__":
